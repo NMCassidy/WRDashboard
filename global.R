49c5f76d3122a92209f7404121993d18d86a3aee
Nicholas Cassidy <nicholas.cassidy@improvementservice.org.uk>
2016-09-28 15:22:50 +0100

    Add data read, tidy

--- global.R
+++ global.R
@@ -1,5 +1,12 @@
 library(leaflet)
 library(RColorBrewer)
 library(mapview)
+library(reshape2)
+##Read shapefiles
 SpPolysDF <- readRDS("Shapes.rds")
 SpPolysLA<- readRDS("LAPolys.rds")
+
+#read and tidy indicator data
+DBdta <- readRDS("dzdata.rds")
+DBdtaMlt <- melt(DBdta, id.vars = "datazone")
+DBdtaMlt[c(4,5)] <- colsplit(DBdtaMlt$variable, "_", c("Indicator", "Date"))
